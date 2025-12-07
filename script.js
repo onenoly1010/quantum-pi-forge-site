093a17a18241c6c3fbd3b785afe30ae053bf662e
onenoly1010 <onenoly1010@gmail.com>
2025-12-07 00:02:18 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for nav links
+document.querySelectorAll("a[href^='#']").forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
